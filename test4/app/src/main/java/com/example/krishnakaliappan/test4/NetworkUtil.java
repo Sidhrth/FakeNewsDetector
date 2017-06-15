8430c440177c5c5b4b8e69fb4babb75ffebd2598
Sidharth Kaliappan <sidharth.kaliappan@gmail.com>
2017-06-15 15:10:35 +0530

    code change
    changed http code

--- test4/app/src/main/java/com/example/krishnakaliappan/test4/NetworkUtil.java
+++ test4/app/src/main/java/com/example/krishnakaliappan/test4/NetworkUtil.java
@@ -1,7 +1,11 @@
 package com.example.krishnakaliappan.test4;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -52,5 +56,33 @@ class NetworkUtil {
         } finally {
             urlConnection.disconnect();
         }
+    }
+
+    public static String httpGet(URL url) throws IOException {
+
+
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        if(conn.getResponseCode() != 200) {
+            throw new IOException(conn.getResponseMessage());
+        }
+
+        Log.d("search", "Connection status = " + conn.getResponseMessage());
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while((line = rd.readLine()) != null) {
+
+            Log.d("search", "Line =" + rd.readLine());
+            sb.append(line+"\n");
+
+        }
+        rd.close();
+
+        conn.disconnect();
+        return sb.toString();
     }
 }
--- test4/app/src/main/java/com/example/krishnakaliappan/test4/resultpage.java
+++ test4/app/src/main/java/com/example/krishnakaliappan/test4/resultpage.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,9 +20,7 @@ import java.util.Scanner;
 
 public class resultpage extends AppCompatActivity {
 
-     public ProgressBar mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
-     public TextView Result = (TextView) findViewById(R.id.results);
-     public TextView Errormessage = (TextView) findViewById(R.id.error_message_display);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,65 +28,130 @@ public class resultpage extends AppCompatActivity {
         setContentView(R.layout.activity_resultpage);
 
         Intent intent = getIntent();
-        String str = intent.getStringExtra("EXTRA_MESSAGE");
-        URL CustomSearchUrl = NetworkUtil.buildUrl(str);
+        final String str = intent.getStringExtra("EXTRA_MESSAGE");
+//        URL CustomSearchUrl = NetworkUtil.buildUrl(str);
+//
+//        new CustomQueryTask().execute(CustomSearchUrl);
+//         ProgressBar mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+         final TextView Result = (TextView) findViewById(R.id.results);
+//         TextView Errormessage = (TextView) findViewById(R.id.error_message_display);
 
-        new CustomQueryTask().execute(CustomSearchUrl);
+        Thread thread = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
 
-
-    }
-
-
-    private void showJsonDataView() {
-        // First, make sure the error is invisible
-        Errormessage.setVisibility(View.INVISIBLE);
-        // Then, make sure the JSON data is visible
-        Result.setVisibility(View.VISIBLE);
-    }
-
-    private void showErrorMessage() {
-        // First, hide the currently visible data
-        Result.setVisibility(View.INVISIBLE);
-        // Then, show the error
-        Errormessage.setVisibility(View.VISIBLE);
-    }
+                try {
 
 
-    public class CustomQueryTask extends AsyncTask<URL, Void, String> {
+                    // looking for
+                    String strNoSpaces = str.replace(" ", "+");
 
-        // COMPLETED (26) Override onPreExecute to set the loading indicator to visible
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-        }
+                    // Your API key
+                    String key="AIzaSyCAajBXGsQKg4FDMBF2Lc1ffl1UtYP5Fzg";
 
-        @Override
-        protected String doInBackground(URL... params) {
-            URL searchUrl = params[0];
-            String CustomSearchResults = null;
-            try {
-                CustomSearchResults = NetworkUtil.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
+                    // Your Search Engine ID
+                    String cx = "013670998315178887075:tstnn39qzcq";
+
+                    String url2 = "https://www.googleapis.com/customsearch/v1?q=" + strNoSpaces + "&key=" + key + "&cx=" + cx + "&alt=json";
+                    Log.d("search", "Url = "+  url2);
+                    String result2 = httpGet(url2);
+
+                    Result.setText(result2);
+
+                }
+                catch(Exception e) {
+                    System.out.println("Error1 " + e.getMessage());
+                }
+
             }
-            return CustomSearchResults;
-        }
 
-        @Override
-        protected void onPostExecute(String CustomSearchResults) {
-            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (CustomSearchResults != null && !CustomSearchResults.equals("")) {
-                // COMPLETED (17) Call showJsonDataView if we have valid, non-null results
-                showJsonDataView();
-                Result.setText(CustomSearchResults);
-            } else {
-                // COMPLETED (16) Call showErrorMessage if the result is null in onPostExecute
-                showErrorMessage();
+
+            private String httpGet(String urlStr) throws IOException {
+
+                URL url = new URL(urlStr);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                if(conn.getResponseCode() != 200) {
+                    throw new IOException(conn.getResponseMessage());
+                }
+
+                Log.d("search", "Connection status = " + conn.getResponseMessage());
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = rd.readLine()) != null) {
+
+                    Log.d("search", "Line =" + rd.readLine());
+                    sb.append(line+"\n");
+
+                }
+                rd.close();
+
+                conn.disconnect();
+                return sb.toString();
             }
-        }
+        });
+
+        thread.start();
+
     }
+
+
+//    private void showJsonDataView() {
+//        // First, make sure the error is invisible
+//        Errormessage.setVisibility(View.INVISIBLE);
+//        // Then, make sure the JSON data is visible
+//        Result.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showErrorMessage() {
+//        // First, hide the currently visible data
+//        Result.setVisibility(View.INVISIBLE);
+//        // Then, show the error
+//        Errormessage.setVisibility(View.VISIBLE);
+//    }
+//
+//
+//    public class CustomQueryTask extends AsyncTask<URL, Void, String> {
+//
+//        // COMPLETED (26) Override onPreExecute to set the loading indicator to visible
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            mLoadingIndicator.setVisibility(View.VISIBLE);
+//        }
+//
+//        @Override
+//        protected String doInBackground(URL... params) {
+//            URL searchUrl = params[0];
+//            String CustomSearchResults = null;
+//            try {
+//                CustomSearchResults = NetworkUtil.httpGet(searchUrl);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return CustomSearchResults;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String CustomSearchResults) {
+//            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
+//            mLoadingIndicator.setVisibility(View.INVISIBLE);
+//            if (CustomSearchResults != null && !CustomSearchResults.equals("")) {
+//                // COMPLETED (17) Call showJsonDataView if we have valid, non-null results
+//                showJsonDataView();
+//               Result.setText(CustomSearchResults);
+//            } else {
+//                // COMPLETED (16) Call showErrorMessage if the result is null in onPostExecute
+//                showErrorMessage();
+//            }
+//        }
+//    }
 }
 
 
